2fa20db0ff7d709cc49c207c4c7179aa2028a73c
Krish8204 <krishvaghasiya8204@gmail.com>
2024-03-31 17:13:03 +0530

    made project

---
+++ server/seeddata.js
@@ -0,0 +1,107 @@
+// seedData.js
+
+module.exports = [
+	{
+		companyName: 'Skoda',
+		distanceCovered: 20000,
+		mileage: 28,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'Sophia Garcia',
+			email: 'Sophia Garcia@example.com',
+		},
+		image: 'https://s1.cdn.autoevolution.com/images/models/SKODA_Kamiq-2023_main.jpg',
+	},
+	{
+		companyName: 'Shelby',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://robbreport.com/wp-content/uploads/2021/02/1-18.jpg',
+	},
+
+	{
+		companyName: 'Volkswagen',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://5.imimg.com/data5/KP/YP/GLADMIN-61801946/volkswagen-vento-car.png',
+	},
+	{
+		companyName: 'jaguar',
+		distanceCovered: 15000,
+		mileage: 23,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'John wick',
+			email: 'john.wick@example.com',
+		},
+		image: 'https://images.hindustantimes.com/auto/img/2022/10/11/600x338/Jaguar_F-Type_75_1665482766305_1665482851154_1665482851154.jpg',
+	},
+	{
+		companyName: 'Hyundai',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://spn-mda.spinny.com/img/YWue3IguT5mCOWstBdjDkg/file.JPG',
+	},
+
+	{
+		companyName: 'Rolls Royce',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://www.rolls-roycemotorcars.com/content/dam/rrmc/marketUK/rollsroycemotorcars_com/4-0-bespoke/components/rolls-royce_GHOST_BESPOKE_D-min.jpg/jcr:content/renditions/cq5dam.web.1920.webp',
+	},
+	{
+		companyName: 'Mercedes',
+		distanceCovered: 10000,
+		mileage: 25,
+		serviceDates: [new Date('2022-01-15'), new Date('2022-03-20')],
+		owner: {
+			name: 'John Doe',
+			email: 'john.doe@example.com',
+		},
+		image: 'https://m.economictimes.com/thumb/msid-103991770,width-1200,height-900,resizemode-4,imgsize-25836/powered-by-a-4-0-litre-v8-engine-the-g-63-grand-edition-offers-a-top-speed-of-220km/h-.jpg',
+	},
+	{
+		companyName: 'Lamborghini',
+		distanceCovered: 8000,
+		mileage: 28,
+		serviceDates: [new Date('2022-02-10'), new Date('2022-04-25')],
+		owner: {
+			name: 'Jane Smith',
+			email: 'jane.smith@example.com',
+		},
+		image: 'https://images.news18.com/ibnlive/uploads/2022/09/aventador-lp-780-4-ultimae-feature-166426477816x9.jpg',
+	},
+
+	{
+		companyName: 'Aston martin',
+		distanceCovered: 10500,
+		mileage: 26,
+		serviceDates: [new Date('2022-10-05'), new Date('2022-12-15')],
+		owner: {
+			name: 'Ava Anderson',
+			email: 'ava.anderson@example.com',
+		},
+		image: 'https://stimg.cardekho.com/images/carexteriorimages/930x620/Aston-Martin/DB12/10185/1696480591668/front-left-side-47.jpg',
+	},
+
+];
